ac9469202c77c5242f909dd8505ea9669d99b529
Gail Bowen <gailbowen1@gmail.com>
2018-05-01 21:53:41 +0100

    Changes

---
+++ MyCode/Controls/ComboBoxWithTextBox.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace MyCode.Controls
+{
+    /// <summary>
+    ///     UserControl where a ComboBox also has an editable TextBox
+    /// </summary>
+
+    public sealed partial class ComboBoxWithTextBox : UserControl
+    {
+        public ComboBoxWithTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public object ComboBoxTextItems
+        {
+            set { comboBox.SetBinding(ItemsControl.ItemsSourceProperty, value as Binding); }
+        }
+
+        public object SelectedComboBoxTextItem
+        {
+            set { textBox.SetBinding(TextBox.TextProperty, value as Binding); }
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            textBox.Text = comboBox.SelectedItem as string;
+        }
+    }
+}
---
+++ MyCode/ViewModels/DoStuffVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCode.ViewModels
+{
+    
+    public class DoStuffVM: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> PetTypes { get; set; }
+
+        private string _selectedPet;
+        
+
+        public string SelectedPet
+        {
+            get { return _selectedPet; }
+            set {
+                _selectedPet = value;
+                if (PetTypes.Contains(_selectedPet) == false)
+                {
+                    PetTypes.Add(_selectedPet);
+                }
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPet)));
+            }
+        }
+
+
+        public DoStuffVM()
+        {
+            //Thanks to https://liftcodeplay.com/2016/02/18/combobox-with-editable-textbox/
+
+            PetTypes = new ObservableCollection<string>();
+
+            PetTypes.Add("Cat");
+            PetTypes.Add("Dog");
+        }
+    }
+}
